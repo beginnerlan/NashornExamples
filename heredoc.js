149201e9459d2c817c1e45023933bed5f40b29ba
sundar <sundar@sundar-TravelMate-5720.(none)>
2014-03-31 09:52:31 +0530

    heredoc and readLine added

---
+++ heredoc.js
@@ -0,0 +1,19 @@
+// Usage: jjs -scripting heredoc.js
+
+// Nashorn supports Shell script like here-documents
+// in -scripting mode. Here-docs are multi-line strings
+// that are possibly interpolated with ${} expressions
+
+var sender = "Buffy the Vampire Slayer";
+var recipient = "Spike";
+ 
+print(<<END
+ 
+Dear ${recipient},
+ 
+I wish you to leave Sunnydale and never return.
+ 
+Not Quite Love,
+${sender}
+ 
+END);
---
+++ readLine.js
@@ -0,0 +1,7 @@
+// Usage: jjs -scripting greeting.js
+
+// readLine prints prompt and reads user response
+var name = readLine("Your name please: ")
+
+// use name interpolated within string
+print("Hello ${name}")
