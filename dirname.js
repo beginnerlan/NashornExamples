6c343e756343eba4db9cd16ad03b8cc0f2723bfa
sundar <sundar@sundar-TravelMate-5720.(none)>
2014-03-31 09:11:02 +0530

    adding dirname.js

---
+++ dirname.js
@@ -0,0 +1,6 @@
+
+// __DIR__ variable is equivalent of `dirname $0` in
+// shell scripts - expands to the directory where
+// the current script is located.
+
+print("This script is in the directory: " + __DIR__)
