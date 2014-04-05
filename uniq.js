6186ac4c8474f7f8a4d982d7fa6d80c1252f5085
sundar <sundar@sundar-TravelMate-5720.(none)>
2014-04-05 14:06:17 +0530

    added uniq.js, Unix uniq like tool

---
+++ uniq.js
@@ -0,0 +1,26 @@
+// Usage: jjs uniq.js
+// or: jjs uniq.jj -- <file>
+
+// Unix uniq-like tool - omit repeated lines
+
+var BufferedReader = Java.type("java.io.BufferedReader")
+var FileReader = Java.type("java.io.FileReader")
+var HashSet = Java.type("java.util.HashSet")
+var InputStreamReader = Java.type("java.io.InputStreamReader")
+var System = Java.type("java.lang.System")
+
+// use object as set - but insertion order preserved
+var uniqueLines = {}
+var reader = arguments.length > 0 ?
+    new FileReader(arguments[0])  :
+    new InputStreamReader(System.in)
+reader = new BufferedReader(reader)
+
+// add unique lines
+reader.lines().forEach(function(line) {
+    uniqueLines[line] = true
+})
+
+for (line in uniqueLines) {
+    print(line)
+}
